d3716ef3db233a8b12456d47e544e8d7e3674cb7
nassimabderahmanback <nassimabderahmanback@gmail.com>
2022-12-04 01:52:12 +0530

    change path

--- controllers/contestParticipentController.js
+++ controllers/contestParticipentController.js
@@ -94,4 +94,69 @@ exports.getContestParticipentsList = async (req, res) => {
   }
 };
 
+exports.updateTicketPrice = async (req, res) => {
+  try {
+    const {
+      userId,
+      ticketId,
+      contestId,
+      price
+    } = req.body;
+
+
+    //change isValid=false in ticket
+    const ticket = await Ticket.findOne({ticketId:ticketId});
+    if (!ticket) {
+      res.status(404).json({ error: "No such Ticket" });
+    }
+
+
+
+
+
+
+    // // check contest name previously use or not
+    // const oldContests = await Contest.find({ name });
+    // if (oldContests.length > 0) {
+    //   return res.status(422).json({ error: "This name is already used" });
+    // }
+
+    // // check contest status.one time can have a active contest
+    // const activeContest = await Contest.find({ status: "Active" });
+    // if (activeContest.length > 0) {
+    //   return res
+    //     .status(422)
+    //     .json({ error: "One contest is already activated" });
+    // }
+
+    // const contestModel = new Contest({
+    //   name: name,
+    //   startDate: startDate,
+    //   endDate: endDate,
+    //   ticketNumbers: ticketNumbers,
+    //   chooseprices: chooseprices,
+    //   mainPrice: mainPrice,
+    //   status: status,
+    // });
+    // const createContest = await contestModel.save();
+
+    // // //generate tickets
+    // // const numberOfTickets = createContest.ticketNumbers;
+    // // const contestId = createContest._id.toString();
+
+    // // for (let i = 0; i < numberOfTickets; i++) {
+    // //   const randomString = makeid(10);
+
+    // //   const ticketModel = new Ticket({
+    // //     ticketId: randomString,
+    // //     contestId: contestId,
+    // //   });
+    // //   await ticketModel.save();
+    // // }
+    // res.status(201).json({ data: createContest });
+  } catch (err) {
+    res.status(422).json({ error: err.message });
+  }
+};
+
 

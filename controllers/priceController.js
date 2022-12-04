cd157ba80fd8b26e0073e91deb80dca3e9bed99b
nassimabderahmanback <nassimabderahmanback@gmail.com>
2022-12-04 14:47:25 +0530

    update

--- controllers/priceController.js
+++ controllers/priceController.js
@@ -9,41 +9,34 @@ exports.addPrice = async (req, res) => {
     });
     const addPrice = await priceModel.save();
 
-    res.status(201).json({ data: addPrice});
+    res.status(201).json({ data: addPrice });
   } catch (err) {
     res.status(422).json({ error: err.message });
   }
 };
 
 exports.getPriceList = async (req, res) => {
-    try {
+  try {
+    const priceList = await Price.find();
+    let priceArray = [];
 
-     
-    
+    for (const element of priceList) {
+      let prices = {
+        id: "",
+        name: "",
+        winningChance: "",
+        checked: "",
+      };
 
-        const priceList = await Price.find();
-        let priceArray = [];
-
-        for (const element of priceList) {
-
-          let prices = {
-            id: "",
-            name: "",
-            winningChance:"" ,
-            checked:""
-          };
-
-            prices.id = element._id.toString();
-            prices.name = element.name;
-            prices.winningChance = element.winningChance;
-            prices.checked = 0
-            priceArray.push(prices);
-      
-        }
-    
-        res.status(200).json(priceArray);
-
-    } catch (err) {
-        res.status(422).json({ error: err.message });
+      prices.id = element._id.toString();
+      prices.name = element.name;
+      prices.winningChance = element.winningChance;
+      prices.checked = 0;
+      priceArray.push(prices);
     }
+
+    res.status(200).json(priceArray);
+  } catch (err) {
+    res.status(422).json({ error: err.message });
+  }
 };

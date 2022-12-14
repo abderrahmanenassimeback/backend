5e3a3787467f20c5f1fe9212ebdbba3f46fef2d2
Hamza1176 <119421963+nassimabderahmanback@users.noreply.github.com>
2022-12-14 20:31:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const apiUrl = "/api/v1";
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
